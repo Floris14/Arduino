941e29a72fe9766d674d0dbfe9a105902bc4b429
Floris14 B <113396@emmauscollege.nl>
2025-05-06 09:44:04 +0000

    6 mei

---
+++ Arduino.ino
@@ -0,0 +1,13 @@
+// variabelen definiëren
+// functies definiëren 
+
+void setup() { // wordt 1x uitgevoerd
+    // init serial monitor
+    // pinmode zetten
+  }
+  
+  void loop() { // wordt oneindig herhaald
+    // sensoren lezen
+    // toestand bepalen
+    // actuatoren zetten
+  }
