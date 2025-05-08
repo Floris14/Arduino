0d094e47861b6639f4abec88f0439099ac94f390
Floris14 B <113396@emmauscollege.nl>
2025-05-08 12:29:36 +0000

    8 mei

--- Arduino.ino
+++ Arduino.ino
@@ -1,13 +1,46 @@
-// variabelen definiëren
-// functies definiëren 
+// pinnen
+const int pinRood = 12;
+const int pinOranje = 10;
+const int pinGroen = 8;
+const int pinKnop = 7;
+
+// variabelen voor actuatoren
+int waardeKnop = LOW;
+bool voetgangerWacht = false;
+
+// variabelen voor toestanden
+const int LEDUIT_KNOPLOS = 1;
+const int LEDAAN_KNOPIN = 2;
+const int LEDAAN_KNOPLOS = 3;
+const int LEDUIT_KNOPIN = 4;
+int toestand = LEDUIT_KNOPLOS;
+
+
 
 void setup() { // wordt 1x uitgevoerd
-    // init serial monitor
-    // pinmode zetten
+
+// init serial monitor
+Serial.begin(9600)
+Serial.println(“start”)
+
+// pinmodes
+pinMode(pinRood  , OUTPUT);
+pinMode(pinOranje, OUTPUT);
+pinMode(pinGroen , OUTPUT); 
+pinMode(pinKnop  , INPUT); 
+
   }
   
   void loop() { // wordt oneindig herhaald
     // sensoren lezen
-    // toestand bepalen
-    // actuatoren zetten
+    if (toestand == LEDUIT_KNOPLOS) {
+        if (millis() - toestandStartTijd > 5000) {
+          toestandStartTijd = millis();
+          toestand = LEDAAN_KNOPIN;
+         }
+      }
+      if (toestand == ORANJE) {
+        if (millis() - toestandStartTijd > 1000)
+  }
+
   }
